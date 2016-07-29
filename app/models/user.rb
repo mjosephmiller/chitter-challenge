40fb9418751151b7a4c887d93865db7dbc03d7ee
Michael <mjosephmiller.uk@gmail.com>
2016-07-29 22:11:50 +0100

    ensures valid email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User
   attr_accessor :password_confirmation
   validates_confirmation_of :password
 
+
   property :id, Serial
-  property :email, String
+  property :email, String, format: :email_address, required: true
   property :password_digest, String, length: 60
   property :name, String
   property :username, String
